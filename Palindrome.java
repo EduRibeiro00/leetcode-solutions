2c3d2716e565ff6981521f3af9666ed4d267a600
Eduardo <eduardo.ribeiro@inesctec.pt>
2020-11-03 13:14:32 +0000

    Added palindrome and squares of sorted array

---
+++ Palindrome.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        
+        while (start < end) {
+            char i = s.charAt(start);
+            char j = s.charAt(end);
+            
+            if (!Character.isLetterOrDigit(i)) {
+                start++;
+                continue;
+            }
+            
+            if (!Character.isLetterOrDigit(j)) {
+                end--;
+                continue;
+            }
+            
+            if (Character.toLowerCase(i) != Character.toLowerCase(j))
+                return false;
+            
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+}
